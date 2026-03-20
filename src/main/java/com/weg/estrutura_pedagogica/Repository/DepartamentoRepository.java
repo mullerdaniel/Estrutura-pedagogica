d971599f59b899b816cf053e2f6e56a1b6329802
Daniel Müller <miladaniel370@gmail.com>
2026-03-20 19:48:42 -0300

    Feat: Finalização da classrepository

---
+++ src/main/java/com/weg/estrutura_pedagogica/Repository/DepartamentoRepository.java
@@ -0,0 +1,4 @@
+package com.weg.estrutura_pedagogica.Repository;
+
+public class DepartamentoRepository {
+}
---
+++ src/main/java/com/weg/estrutura_pedagogica/Repository/FuncionarioRepository.java
@@ -0,0 +1,4 @@
+package com.weg.estrutura_pedagogica.Repository;
+
+public interface FuncionarioRepository {
+}
